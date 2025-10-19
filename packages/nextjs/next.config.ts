680ef759fb72f004ca68bfa128992edf25929652
Subhankar <officialsubhankar01@gmail.com>
2025-10-19 21:29:14 +0530

    fix: correct Next.js image remotePatterns config

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
         hostname: "pbs.twimg.com",
       },
     ],
